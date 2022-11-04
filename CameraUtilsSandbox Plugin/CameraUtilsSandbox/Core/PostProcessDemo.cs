352475cffd58d0271494e2d36f9aee398169a172
Reezonate <xxxx359xxxx@gmail.com>
2022-11-04 21:41:04 +0900

    ICameraEffect post process

--- CameraUtilsSandbox Plugin/CameraUtilsSandbox/Core/PostProcessDemo.cs
+++ CameraUtilsSandbox Plugin/CameraUtilsSandbox/Core/PostProcessDemo.cs
@@ -7,18 +7,38 @@ using Zenject;
 
 namespace CameraUtilsSandbox {
     [UsedImplicitly]
-    public class PostProcessDemo : IInitializable, IDisposable {
+    public class PostProcessDemo : IInitializable, IDisposable, ICameraEffect {
+        #region Init / Dispose
+
         private CommandBuffer _commandBuffer;
 
         public void Initialize() {
             _commandBuffer = new CommandBuffer();
             _commandBuffer.DrawMesh(BundleLoader.PostProcessQuad, Matrix4x4.identity, BundleLoader.InvertPostProcessMaterial);
-            CamerasManager.RegisterCommandBuffer(CameraEvent.AfterImageEffects, _commandBuffer, CameraFlags.HMD, CameraFlags.Mirror);
+            CamerasManager.RegisterCameraEffect(this);
         }
 
         public void Dispose() {
-            CamerasManager.UnRegisterCommandBuffer(_commandBuffer);
             _commandBuffer.Release();
+            CamerasManager.UnRegisterCameraEffect(this);
         }
+
+        #endregion
+
+        #region ICameraEffect
+
+        public bool IsSuitableForCamera(RegisteredCamera registeredCamera) {
+            return registeredCamera.CameraFlags.HasFlag(CameraFlags.FirstPerson) && !registeredCamera.CameraFlags.HasFlag(CameraFlags.Mirror);
+        }
+
+        public void OnAddedToCamera(RegisteredCamera registeredCamera) {
+            registeredCamera.Camera.AddCommandBuffer(CameraEvent.AfterImageEffects, _commandBuffer);
+        }
+
+        public void OnRemovedFromCamera(RegisteredCamera registeredCamera) {
+            registeredCamera.Camera.RemoveCommandBuffer(CameraEvent.AfterImageEffects, _commandBuffer);
+        }
+
+        #endregion
     }
 }
